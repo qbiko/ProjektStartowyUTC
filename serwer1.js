40409bffc7d24037a46eb3f14e4992791c201889
qbiko <kubiko13@gmail.com>
2016-09-05 10:26:08 +0200

    Jesteśmy na dobrej drodze

---
+++ serwer1.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+
+// Create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.use(express.static('public'));
+
+app.get('/index.html', function (req, res) {
+   res.sendFile( __dirname + "/" + "index.html" );
+})
+
+app.post('/process_post', urlencodedParser, function (req, res) {
+
+   // Prepare output in JSON format
+   response = {
+       imie:req.body.imie,
+       nazwisko:req.body.nazwisko
+   };
+   console.log(response);
+   res.end(JSON.stringify(response));
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Serwer dziala http://http://127.0.0.1:8081/")
+
+})
